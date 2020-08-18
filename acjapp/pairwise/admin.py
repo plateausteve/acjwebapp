b240647e6f639c54da8da44594530e90b499c50d
Steve Heil <plateausteve@gmail.com>
2020-08-17 18:50:46 -0600

    Added Export_Import with admin support and took first steps in exploring PariedCompCalc, though that is a bit advanced for me at this moment.

--- acjapp/pairwise/admin.py
+++ acjapp/pairwise/admin.py
@@ -1,7 +1,15 @@
 from django.contrib import admin
 from .models import Script, Comparison, Set
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin
 
-admin.site.register(Comparison)
+class ComparisonResource(resources.ModelResource):
+    class Meta:
+        model = Comparison
+
+class ComparisonAdmin(ImportExportModelAdmin):
+    resource_class = ComparisonResource
+
+admin.site.register(Comparison, ComparisonAdmin)
 admin.site.register(Script)
 admin.site.register(Set)
-# Register your models here.
