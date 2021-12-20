14e2fded3c42da7473bc8fc309a11ae0327a2ed6
Steve Heil <plateausteve@gmail.com>
2021-12-20 15:50:10 -0700

    deleted nested acjsite folder keeping new acjapp folder and updated pip file

--- acjsite/pairwise/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Script, Comparison, Set
-
-admin.site.register(Comparison)
-admin.site.register(Script)
-admin.site.register(Set)
-# Register your models here.
--- acjsite/pairwise/apps.py
+++
@@ -1,4 +0,0 @@
-from django.apps import AppConfig
-
-class PairwiseConfig(AppConfig):
-    name = 'pairwise'
