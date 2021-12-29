d20eadb9b9786d054ab0b2323288b297cc34412c
Steve Heil <plateausteve@gmail.com>
2021-12-28 17:01:00 -0700

    deleted frontend script add-edit-detail templates

--- acjapp/pairwise/urls.py
+++ acjapp/pairwise/urls.py
@@ -25,7 +25,4 @@ urlpatterns = [
     path('script_list/', views.script_list, name='scripts'),
     path('update/', views.update, name='update'),
     path('script_chart/', views.script_chart_view, name='script_chart'),
-    path('script/add', views.script_add, name='script_add'),
-    path('script/<int:pk>/edit/', views.script_edit, name='script_edit'),
-    path('script/<int:pk>/', views.script_detail, name='script_detail'),
 ]
